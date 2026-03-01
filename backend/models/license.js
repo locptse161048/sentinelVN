cc5297ceb3247d851ef04f42b2b7027f5501b9a1
Sentinel Developer <dev@sentinel.local>
2026-03-01 22:32:50 +0700

    thêm chức năng thanh toán lần 15

--- backend/models/license.js
+++ backend/models/license.js
@@ -5,7 +5,7 @@ const LicenseSchema = new mongoose.Schema({
 	clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', required: true },
 	key: { type: String, unique: true, required: true },
 	plan: { type: String, enum: ['PREMIUM', 'PRO'], required: true },
-	ammount: { type: Number, required: true },
+	amount: { type: Number, required: true },
 	createdAt: { type: Date, default: Date.now },
 	expiresAt: { type: Date, required: true },
 });
